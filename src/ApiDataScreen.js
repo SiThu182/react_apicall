0967dcbd910248e7025e7ef3930171c41184e6f8
unknown <SiThu Aung>
2020-12-19 11:09:38 +0630

    first commit

---
+++ src/ApiDataScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+const ApiDataScreen = () => {
+
+    useEffect(() => {
+       
+    }, [])
+
+         return (
+            <View style={{flex: 1,alignItems:'center',justifyContent:'center'}}>
+                <Text> Api Data Screen </Text>
+            </View>
+        )
+}
+
+export default ApiDataScreen;
